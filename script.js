e1b1653d70894d43030fbf57854d0ae28c931340
Treya St Amant <22247165+treyastamant@users.noreply.github.com>
2023-07-27 06:17:17 -0600

    Refactor using class

--- script.js
+++ script.js
@@ -25,139 +25,134 @@ const modalControl = (() => {
 })();
 
 //create book - make class
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
-
 
 const libraryController = (() => {  
   let myLibrary = [];
   let count = 0;
 
   const addBook = document.querySelector('.add-book');
-addBook.addEventListener('click', (e) => {
-  e.preventDefault();
- //take users input and store new book objects into an array
-  const title = document.getElementById('title');
-  const author = document.getElementById('author');
-  const pages = document.getElementById('pages');
-  const read = document.getElementsByName('read');
-  let readValue;
+  addBook.addEventListener('click', (e) => {
+    e.preventDefault();
+    //take users input and store new book objects into an array
+    const title = document.getElementById('title');
+    const author = document.getElementById('author');
+    const pages = document.getElementById('pages');
+    const read = document.getElementsByName('read');
+    let readValue;
 
-  for (i = 0; i < read.length; i++) {
-    if (read[i].checked) {
-      readValue = read[i].value;
+    for (i = 0; i < read.length; i++) {
+      if (read[i].checked) {
+        readValue = read[i].value;
+      }
     }
-  }
-
-  if (title.value.length === 0) {
-    title.classList.add('invalid');
-  }
-  if (author.value.length === 0) {
-    author.classList.add('invalid');
-  } 
-  if (pages.value.length === 0) {
-    pages.classList.add('invalid');
-  } else {
-    const book = new Book(title.value, author.value, pages.value, readValue);
-    myLibrary.push(book);
-    displayBookInLibrary(book);
-    modalControl.closeModal();
-    document.getElementById('form').reset();
-  }
-});
-
-title.addEventListener('focus', () => {
-  title.classList.remove('invalid');
-});
-
-author.addEventListener('focus', () => {
-  author.classList.remove('invalid');
-});
-
-pages.addEventListener('focus', () => {
-  pages.classList.remove('invalid');
-});
-//end add book
-
-
-function displayBookInLibrary() {
-  //Display new book on the page.
-  const bookContainer = document.querySelector('.books-container');
-
-  const bookCard = document.createElement('div');
-  bookCard.classList.add('book-card');
-  bookCard.setAttribute('data-num', count);
-  bookContainer.appendChild(bookCard);
-
-  const titleAuthorGroup = document.createElement('div');
-  titleAuthorGroup.classList.add('title-author-group');
-  bookCard.appendChild(titleAuthorGroup);
-
-  const bookTitle = document.createElement('h3');
-  bookTitle.textContent = `${myLibrary[count].title}`;
-  titleAuthorGroup.appendChild(bookTitle);
-
-  const bookAuthor = document.createElement('p');
-  bookAuthor.textContent = `by ${myLibrary[count].author}`;
-  bookAuthor.classList.add('author');
-  titleAuthorGroup.appendChild(bookAuthor);
-
-  const pagesReadGroup = document.createElement('div');
-  pagesReadGroup.classList.add('pages-read-group');
-  bookCard.appendChild(pagesReadGroup);
-
-  const bookPages = document.createElement('p');
-  bookPages.textContent = `${myLibrary[count].pages} pages`;
-  pagesReadGroup.appendChild(bookPages);
-
-  if (myLibrary[count].read === "Not Read") {
-    const markRead = document.createElement('button');
-    markRead.textContent = `Mark Read`;
-    markRead.classList.add('primary-button');
-    pagesReadGroup.appendChild(markRead);
-
-    markRead.addEventListener('click', () => {
-      const bookCount = markRead.parentElement.parentElement.dataset.num;
-      myLibrary[bookCount].read = "Read";
-      
-      //Delete 'Mark Read' button
-      markRead.remove();
-
-      //Show 'Read'
-      const markBookRead = document.createElement('p');
-      markBookRead.innerHTML = `<span class="material-symbols-outlined">
-      check_circle
-      </span> ${myLibrary[bookCount].read}`;
-      pagesReadGroup.appendChild(markBookRead);
-    })
-
-  } else {
-    const bookRead = document.createElement('p');
-    bookRead.innerHTML = `<span class="material-symbols-outlined">
-    check_circle
-    </span> ${myLibrary[count].read}`;
-    pagesReadGroup.appendChild(bookRead);
-  }
-
-  
-
-  const deleteBook = document.createElement('button');
-  deleteBook.textContent = `Delete`;
-  deleteBook.classList.add('delete');
-  bookCard.appendChild(deleteBook);
-
-  count++;
-
-  console.log(deleteBook);
-  deleteBook.addEventListener('click', () => {
-    deleteBook.parentElement.remove();
+    if (title.value.length === 0) {
+      title.classList.add('invalid');
+    }
+    if (author.value.length === 0) {
+      author.classList.add('invalid');
+    } 
+    if (pages.value.length === 0) {
+      pages.classList.add('invalid');
+    } else {
+      const book = new Book(title.value, author.value, pages.value, readValue);
+      myLibrary.push(book);
+      displayBookInLibrary(book);
+      modalControl.closeModal();
+      document.getElementById('form').reset();
+    }
   });
-}
 
+  title.addEventListener('focus', () => {
+    title.classList.remove('invalid');
+  });
+
+  author.addEventListener('focus', () => {
+    author.classList.remove('invalid');
+  });
+
+  pages.addEventListener('focus', () => {
+    pages.classList.remove('invalid');
+  });
+
+
+  function displayBookInLibrary() {
+    //Display new book on the page.
+    const bookContainer = document.querySelector('.books-container');
+
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('book-card');
+    bookCard.setAttribute('data-num', count);
+    bookContainer.appendChild(bookCard);
+
+    const titleAuthorGroup = document.createElement('div');
+    titleAuthorGroup.classList.add('title-author-group');
+    bookCard.appendChild(titleAuthorGroup);
+
+    const bookTitle = document.createElement('h3');
+    bookTitle.textContent = `${myLibrary[count].title}`;
+    titleAuthorGroup.appendChild(bookTitle);
+
+    const bookAuthor = document.createElement('p');
+    bookAuthor.textContent = `by ${myLibrary[count].author}`;
+    bookAuthor.classList.add('author');
+    titleAuthorGroup.appendChild(bookAuthor);
+
+    const pagesReadGroup = document.createElement('div');
+    pagesReadGroup.classList.add('pages-read-group');
+    bookCard.appendChild(pagesReadGroup);
+
+    const bookPages = document.createElement('p');
+    bookPages.textContent = `${myLibrary[count].pages} pages`;
+    pagesReadGroup.appendChild(bookPages);
+
+    if (myLibrary[count].read === "Not Read") {
+      const markRead = document.createElement('button');
+      markRead.textContent = `Mark Read`;
+      markRead.classList.add('primary-button');
+      pagesReadGroup.appendChild(markRead);
+
+      markRead.addEventListener('click', () => {
+        const bookCount = markRead.parentElement.parentElement.dataset.num;
+        myLibrary[bookCount].read = "Read";
+        
+        //Delete 'Mark Read' button
+        markRead.remove();
+
+        //Show 'Read'
+        const markBookRead = document.createElement('p');
+        markBookRead.innerHTML = `<span class="material-symbols-outlined">
+        check_circle
+        </span> ${myLibrary[bookCount].read}`;
+        pagesReadGroup.appendChild(markBookRead);
+      })
+
+    } else {
+      const bookRead = document.createElement('p');
+      bookRead.innerHTML = `<span class="material-symbols-outlined">
+      check_circle
+      </span> ${myLibrary[count].read}`;
+      pagesReadGroup.appendChild(bookRead);
+    }
+
+    const deleteBookBtn = document.createElement('button');
+    deleteBookBtn.textContent = `Delete`;
+    deleteBookBtn.classList.add('delete');
+    bookCard.appendChild(deleteBookBtn);
+
+    count++;
+
+    deleteBookBtn.addEventListener('click', () => {
+      deleteBookBtn.parentElement.remove();
+    });
+  }
 })();
 
 
